a284053f0abb28949d5d7fbe93d061b5d8d8acbb
tapan77777 <72134688+tapan77777@users.noreply.github.com>
2024-11-16 20:03:00 +0530

    Use dynamic port from environment variables

--- backend/server.js
+++ backend/server.js
@@ -12,5 +12,5 @@ app.use(express.json());
 
 app.use('/api/menu-items', menuItemsRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
