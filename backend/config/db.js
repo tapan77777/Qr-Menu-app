9fc483e0d647218410c5e3fe9a6f4e52b0a54c37
tapan77777 <72134688+tapan77777@users.noreply.github.com>
2024-11-16 19:43:24 +0530

    Removed deprecated MongoDB driver options

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose');
+
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log('MongoDB connected...');
     } catch (error) {
-        console.error(error.message);
+        console.error('Error connecting to MongoDB:', error.message);
         process.exit(1);
     }
 };
+
 module.exports = connectDB;
